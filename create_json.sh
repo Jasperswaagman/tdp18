7f71a9bb6ae0806371b3171dd36001005fb46eca
Jasper Swaagman <jasperswaagman@gmail.com>
2018-07-04 19:46:50 +0200

    init

---
+++ create_json.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+route_id=$(tail -10 "$1" | grep route_ | tr -dc '0-9')
+route_id=$(echo $route_id + 1 | bc | awk '{printf $0}') # needs a padding zero for 1-9
+id=$(tail -10 "$1" | grep -m1 id | tr -dc '0-9')
+id=$(($id + 1))
+
+echo -en "Name: "; read name
+echo -en "RWGPS: "; read rwgps_id
+echo -en "velo id: "; read velo_id
+echo -en "segments: "; read segments
+
+# get hoogtemeters: 
+rwgps_html=$(wget -qO- https://ridewithgps.com/routes/"$rwgps_id")
+distance=$(grep -m 1 -A 3 hide_unless_distance <<< $rwgps_html | tail -1 | sed 's/ km$//')
+elevation=$(grep -m 1 txtAscent <<< $rwgps_html | tr -dc '0-9')
+
+segments_json='"segments": {'
+i=1; b=""
+for s in $segments; do
+  seg_html=$(wget -qO- https://veloviewer.com/segments/"$s")
+  seg_length=$(grep -m1 '="distance"' <<< $seg_html | perl -wnE 'say /\d+\.\d+/g')
+  seg_avggrade=$(grep -m1 '="grade"' <<< $seg_html | perl -wnE 'say /\d+\.\d+/g')
+  b="\"$i\": { \"seg_id\": $s, \"Length\": $seg_length, \"avg_grade\": $seg_avggrade },"
+  body+="$b"
+  i=$((i + 1))
+done
+body=$(sed 's/,$//' <<< $body)
+segments_json+="$body},"
+
+json='  "route_'$route_id'": {
+    "id": '$id',
+    "name": "'$name'",
+    "distance": '$distance',
+    "elevation": '$elevation',
+    '$segments_json'
+    "link_velo": "https://veloviewer.com/routes/'$velo_id'",
+    "link_rwgps": "https://ridewithgps.com/routes/'$rwgps_id'"
+  },'
+
+echo "$json" >> "$1"
